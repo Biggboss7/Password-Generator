75d6c17894743834bf70c8ca1942c44efdfe3a8b
Bigboss <mtse431@gmail.com>
2023-09-01 16:35:05 +0700

    Builds Structures and Functions to handle Error Form Validation

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Targeted Elements
-
-// Showing Length of Password
 const rangeInput = document.querySelector("input[type='range']");
 const passwordLengthEl = document.getElementById("col-passwordLengthEl");
+const generateBtnEl = document.getElementById("btn--generateEl");
+const lengthErrorEl = document.querySelector(".length--error");
+const optionErrorEl = document.querySelector(".option--error");
+const checkBoxInputEl = document.querySelectorAll("input[type='checkbox']");
 
+// Showing Length of Password
 function inputRangeProgress() {
   rangeInput.style.background = `linear-gradient(to right, #A4FFAF ${
     (rangeInput.value / 20) * 100
   }%, #18171F ${(rangeInput.value / 20) * 100}%)`;
   passwordLengthEl.textContent = rangeInput.value;
 }
-
 rangeInput.addEventListener("input", inputRangeProgress);
+
+// Form Validation
+function checkError() {
+  if (rangeInput.value === "0") lengthErrorEl.classList.add("error");
+  else lengthErrorEl.classList.remove("error");
+
+  if ([...checkBoxInputEl].some(box => box.checked))
+    optionErrorEl.classList.remove("error");
+  else optionErrorEl.classList.add("error");
+}
